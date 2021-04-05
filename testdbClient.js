595739cb7d0a5bfd4bd2b52b68c2621b8495c400
”kensuke” <kensuke1202left@icloud.com>
2021-04-05 14:15:16 +0900

    first commit

---
+++ testdbClient.js
@@ -0,0 +1,31 @@
+const gF = require('./generalFunctions')
+const testData = require('../mytrainer-app-front/database/test-data.json')
+/**
+ * @param {Object} testData
+ * @returns {Number} num
+ */
+exports.count = () => {
+    const data = testData
+    const count = Object.keys(data).length
+    // console.log(testData)
+    return count
+}
+
+/**
+ * 
+ * @param {Array} NumArray 
+ * @returns {Array} ramdomDatas
+ */
+exports.getRamdomData = (NumArray) => {
+    const ramdomDatas = []
+    const dataArray = testData.slice()
+    for(let i = 0; i < NumArray.length; i++){
+        dataArray.forEach((object) => {
+            if(Number(object.id) === NumArray[i]){
+                ramdomDatas.push(object)
+                return 
+            }
+        })
+    }
+    return ramdomDatas
+}
